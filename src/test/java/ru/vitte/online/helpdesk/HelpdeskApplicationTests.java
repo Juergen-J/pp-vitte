3c2c820248f78463b84cf62bd0ad192277e97d90
Juergen-J <yurk.jur@gmail.com>
2024-01-20 19:14:30 +0100

    project init

---
+++ src/test/java/ru/vitte/online/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vitte.online.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
